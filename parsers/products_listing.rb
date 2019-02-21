6e866a446b4e524d6e2c46124606812672e124ae
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-21 15:56:52 +0100

    Update scraper.

--- parsers/products_listing.rb
+++ parsers/products_listing.rb
@@ -37,12 +37,25 @@ else
 end
 
 
+headers = {
+
+    "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding" => "gzip, deflate, br",
+    "Accept-Language" => "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,de-DE;q=0.6,de;q=0.5",
+    "Cache-Control" => "max-age=0",
+    "Connection" => "keep-alive",
+    "Host" => "www.submarino.com.br",
+    "Upgrade-Insecure-Requests" => "1",
+    "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
+}
+
 products.each_with_index do |product, i|
 
   pages << {
       page_type: 'product_details',
       method: 'GET',
       url: "https://www.submarino.com.br/produto/#{product['id']}/?searchkeyword=#{page['vars']['search_term']}&searchpage=#{page['vars']['page']}",
+      headers:headers,
       vars: {
           'input_type' => page['vars']['input_type'],
           'search_term' => page['vars']['search_term'],
