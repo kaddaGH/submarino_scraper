cf703b6ddd0186a8fd90e80764fc720afbeb44f5
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-21 12:24:40 +0100

    Update seeder.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,5 +1,5 @@
 pages << {
-    page_type: 'products_search',
+    page_type: 'products_listing',
     method: 'GET',
     url: "https://mystique-v2-submarino.b2w.io/search?offset=0&sortBy=topSelling&source=omega&filter=%7B%22id%22%3A%22category.id%22%2C%22value%22%3A%22300228%22%2C%22fixed%22%3Atrue%7D&limit=24&suggestion=false",
     vars: {
@@ -14,7 +14,7 @@ search_terms = ['Red Bull', 'RedBull', 'Energético', 'Energéticos']
 search_terms.each do |search_term|
 
   pages << {
-      page_type: 'products_search',
+      page_type: 'products_listing',
       method: 'GET',
       url: "https://mystique-v2-submarino.b2w.io/search?content=#{search_term}&offset=0&sortBy=relevance&source=nanook&limit=24&suggestion=false&c_b2wSid=469.846063031196620192141153145&c_b2wUid=va_201913311927_801.2812056579871",
       vars: {
